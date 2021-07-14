cb514ef06cb2c85432a7483d2add83f0487bcf7f
yaoyanli <951934346@qq.com>
2021-07-14 11:35:51 +0800

    update for testing webhook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World again!";
+    private final String message = "Hello World and testing webhook!";
 
     public App() {}
 
